267c222fb91bb43077225ba0707000132925f4cd
takanori-M <kohunzin@gmail.com>
2024-03-19 10:59:42 +0900

    追加修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'package:chatapp_2/shared/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+if(kIsWeb) {
+  // run the initialization for web
+  await Firebase.initializeApp(options: FirebaseOptions(
+    apiKey: Constants.apiKey,
+    appId: Constants.appId,
+    messagingSenderId: Constants.messagingSenderId,
+    projectId: Constants.projectId));
+}
+else {
+  // run the initialization for andoroid, ios
   await Firebase.initializeApp();
+}
+
   runApp(const MyApp());
 }
 
---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:858176062189:web:63b0918952796101c2c2e4";
+  static String apiKey = "AIzaSyDat4pFvlHLBRr-WV0UmU0opnZm-JMXfNA";
+  static String messagingSenderId = "858176062189";
+  static String projectId = "chatapp-2-5efe8";
+}
